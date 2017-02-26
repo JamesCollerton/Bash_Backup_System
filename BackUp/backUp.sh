f5320bfe01486c579f64ad0d63bc8691722b4858
James Collerton <jamescollerton@live.co.uk>
2017-02-26 13:48:28 +0000

    Removed echo and added print messages

--- BackUp/backUp.sh
+++ BackUp/backUp.sh
@@ -7,16 +7,44 @@ cd "$parent_path"
 source ../bin/formatting.sh
 source ../Configurations/config.sh
 
+# Prints the message when we start.
+function printBackUpStartMessage {
+
+	newline
+
+	echo "---------------------------------------"
+	echo "Back Up Beginning"
+	echo "---------------------------------------"
+
+	newline
+
+}
+
 # Used to compress the folder we want to back up.
 function compressLocationFolder {
 
 	backUpDirectoryNoSlash="${backUpDirectory:1:${#backUpDirectory}}"
-	echo "$backUpDirectoryNoSlash"
+
+	tar -cvzf $(date +%Y%m%d-%H%M%S).tar.gz -C / "$backUpDirectoryNoSlash";
+
+}
+
+function printBackUpEndMessage {
+	
 	newline
 
-	backUpDirectoryDot=."$backUpDirectory"
-	echo "$backUpDirectoryDot"
+	echo "---------------------------------------"
+	echo "Back Up Finished"
+	echo "---------------------------------------"
+
 	newline
+
+}
+
+# Used to compress the folder we want to back up.
+function compressLocationFolder {
+
+	backUpDirectoryNoSlash="${backUpDirectory:1:${#backUpDirectory}}"
 
 	tar -cvzf $(date +%Y%m%d-%H%M%S).tar.gz -C / "$backUpDirectoryNoSlash";
 
@@ -25,7 +53,9 @@ function compressLocationFolder {
 # Runs main function for backing up
 function backUpMain {
 
+	printBackUpStartMessage
 	compressLocationFolder
+	printBackUpEndMessage
 
 }
 
