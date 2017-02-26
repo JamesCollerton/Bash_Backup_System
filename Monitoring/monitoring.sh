4882b59eca1f5b595bf4eab30cddd332f4ee1a2b
James Collerton <jamescollerton@live.co.uk>
2017-02-26 17:28:43 +0000

    Added basic monitoring

--- Monitoring/monitoring.sh
+++ Monitoring/monitoring.sh
@@ -9,16 +9,39 @@ source ../Configurations/config.sh
 
 # Check the size of the back up location
 function checkBackUpLocationSize {
-	
+
+	newline
+
+	echo "Current Back Up Location Size"
+
+	newline
+
+	local backUpLocation="$backUpLocation"
+
+	du -sh "$backUpLocation"	
+
 }
 
 # Check how many files have been backed up
 function checkNumberOfBackUps {
 
+	newline
+	
+	echo "Current Number of Backed Up Files"
+
+	newline
+
+	local backUpLocation="$backUpLocation"
+
+	ls -l | wc -l	
+
 }
 
 # Main for the monitoring section
 function monitoringMain {
+
+	checkBackUpLocationSize
+	checkNumberOfBackUps
 
 }
 
